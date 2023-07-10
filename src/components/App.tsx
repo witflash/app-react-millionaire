69974ee0deb7d411e8f630cb50aae21a2874970d
Andrii Mambyk <a.mambyk@weplay.tv>
2023-07-11 01:08:41 +0300

    [app] remove mocked text
    [app] remove mocked text
    
    [app] remove mocked text
    
    [app] remove mocked text
    
    [app] remove mocked text
    
    [global] add pre-commit
    
    [app] test
    
    [app] test
    
    [app] test
    
    [app] test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,12 @@
 import React from 'react';
 import { Loader } from 'components/Loader';
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Loader />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <Loader />{' '}
       </header>
     </div>
   );
