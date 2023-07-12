4fa6476d0d673e0791fd3547aeec9e5661bb2af1
Andrii Mambyk <a.mambyk@weplay.tv>
2023-07-13 02:13:26 +0300

    [global] reformat components and features

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from 'react-dom/client';
 import { reduxStore } from 'store';
 import { Provider } from 'react-redux';
-import App from 'components/App';
+import App from './App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
